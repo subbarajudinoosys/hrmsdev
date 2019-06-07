260a5dadc04c058bdfe794155da76401013403de
mounikadanduu <mounikadanduu@users.noreply.github.com>
2019-06-07 12:05:41 +0530

    addedby mounika

---
+++ App_Code/Entites/DALExeperienceList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EntityManager;
+using System.Data;
+using System.Collections;
+
+
+namespace DataManager
+{
+    /// <summary>
+    /// Summary description for DALExeperienceList
+    /// </summary>
+    public class DALExeperienceList : DataUtilities
+
+    {
+        public DataSet GetEmpExperienceList(EmpExperience objEmpExeList)
+        {
+            Hashtable htparams = new Hashtable
+                                                  {
+                                                      {"inS_No",objEmpExeList.S_No},
+                                                      {"inEmp_id",objEmpExeList.Emp_id},
+                                                      {"inCompanyName",objEmpExeList.CompanyName},
+                                                      {"inFromDate",objEmpExeList.FromDate},
+                                                      {"inToDate",objEmpExeList.ToDate},
+                                                      {"inDesignation",objEmpExeList.Designation},
+                                                      {"inTechnology",objEmpExeList.Technology},
+                                                      {"inProjectTitles",objEmpExeList.ProjectTitles},
+                                                       {"inIsActive",objEmpExeList.IsActive},
+                                                      {"in_OpName",objEmpExeList.OpName},
+                                                   };
+            return ExecuteDataSet("EmpExperience", htparams);
+        }
+    }
+}
