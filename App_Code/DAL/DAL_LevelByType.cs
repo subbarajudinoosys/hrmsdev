5c1e05d8305ad92ffadc3b854580ec789fa07633
Prathyuvarma <51368418+Prathyuvarma@users.noreply.github.com>
2019-06-11 17:25:12 +0530

    updated by prathyusha

---
+++ App_Code/DAL/DAL_LevelByType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EntityManager;
+using System.Data;
+using System.Collections;
+
+/// <summary>
+/// Summary description for DAL_LevelByType
+/// </summary>
+namespace DataManager
+{
+    public class DAL_LevelByType:DataUtilities
+    {
+        public DataSet GetLevelByType(int Edu_id)
+        {
+            Hashtable htparams = new Hashtable
+            {
+                {"@EduType_id",Edu_id},
+                //{"@EduType_Name",objEmpEdu.EduType_Name}
+            };
+            return ExecuteDataSet("Education_Levelbytype", htparams);
+        }
+    }
+}
