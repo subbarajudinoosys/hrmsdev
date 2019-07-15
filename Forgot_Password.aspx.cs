c778c8827b0c54ecc3692294503cb81460583dec
Prathyuvarma <51368418+Prathyuvarma@users.noreply.github.com>
2019-07-15 15:12:14 +0530

    updated

--- Forgot_Password.aspx.cs
+++ Forgot_Password.aspx.cs
@@ -12,13 +12,15 @@ using System.Globalization;
 public partial class Forgot_Password : System.Web.UI.Page
 {
     DOUtility objDOUty = new DOUtility();
+    string EmployeeId = string.Empty;
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
         {
-
+           
         }
     }
+    
     protected void btnForgot_Click(object sender, EventArgs e)
     {
         try
@@ -32,23 +34,27 @@ public partial class Forgot_Password : System.Web.UI.Page
                     //DataSet ds = objBALUsers.GetConfig();
                     //if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
                     //{
+
+                    //Session["mailEmpId"] = ds.Tables[0].Rows[0]["emp_id"].ToString();
+                    string emp_id=ds.Tables[0].Rows[0]["emp_id"].ToString();
                     string SmtpServer = "smtp.gmail.com";
-                    int SmtpPort = 587;
+                    int SmtpPort = 25;
                     // string SmtpServer = "";
                     //int SmtpPort = Convert.ToInt32(ds.Tables[0].Rows[0]["con_smtp_port"].ToString());
                     string MailFrom = "testingformail12@gmail.com";
                     string DisplayNameFrom = "HRMS";
                     string FromPassword = "testing123";
                     string MailTo = txtemailId.Text.Trim();
-                    string DisplayNameTo = "";
+                    string DisplayNameTo = string.Empty;
                     string MailCc = "mounikadandu35@gmail.com";
-                    string DisplayNameCc = "";
-                    string MailBcc = "";
+                    string DisplayNameCc = string.Empty;
+                    string MailBcc = string.Empty;
                     string Subject = "HRMS";
+                    string body = "/admin//ChangePassword.aspx";
                     string MailText;
-                    string Attachment = "";
+                    string Attachment = string.Empty;
                     string Password=ds.Tables[0].Rows[0][0].ToString().Trim();
-                    MailCc = "";
+                    MailCc = string.Empty;
 
                     MailText = "<table align='center' border='0' cellpadding='0' cellspacing='0' style='border-collapse: collapse; width: 100%; max-width: 600px;' class='content'>"
                     + "<tr>"
@@ -66,16 +72,16 @@ public partial class Forgot_Password : System.Web.UI.Page
                     + " <td align='center' bgcolor='#ffffff' style='padding: 75px 20px 40px 20px; color: #555555; font-family: Arial, sans-serif; font-size: 20px; line-height: 30px; border-bottom: 1px solid #f6f6f6;'>"
                     + "     <b>You Credentials is </b><br/>"
                         //+ "    and One Of The Advisor We Will Contact To You."
-                     + "Your Login Id : " + ds.Tables[0].Rows[0][1].ToString().Trim() + "<br/>" + " Password: " + objDOUty.Decrypts( Password,true)
-
+                     + "Login Id : " + ds.Tables[0].Rows[0][1].ToString().Trim() + "<br/>" + " Password: " + objDOUty.Decrypts( Password,true)
+                     + "<br/> To change password click <a href='http://hrmsdev.dinoosystech.com/admin/ChangePassword.aspx?key='" + objDOUty.Encrypts(emp_id,true) + "'target='_blank'>here</a>"
                     + " </td>"
                  + "</tr>"
                 + " <tr>"
-                   + "  <td align='center' bgcolor='#f9f9f9' style='padding: 30px 20px 30px 20px; font-family: Arial, sans-serif;'>"
-                    + "     <table bgcolor='#1ABC9C' border='0' cellspacing='0' cellpadding='0' class='buttonwrapper'>"
+                   //+ "  <td align='center' bgcolor='#f9f9f9' style='padding: 30px 20px 30px 20px; font-family: Arial, sans-serif;'>"
+                    //+ "     <table bgcolor='#1ABC9C' border='0' cellspacing='0' cellpadding='0' class='buttonwrapper'>"
                           + "   <tr>"
-                            + "     <td align='center' height='50' style=' padding: 0 25px 0 25px; font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; background-color: #bd1f2d;' class='button'>"
-                                 + "  <a href='http://hrmsdev.dinoosystech.com/login.aspx' style='color: #ffffff; text-align: center; text-decoration: none;'>HRMS DINOOSYS</a>"
+                            //+ "     <td align='center' height='50' style=' padding: 0 25px 0 25px; font-family: Arial, sans-serif; font-size: 16px; font-weight: bold; background-color: #bd1f2d;' class='button'>"
+                                   
                               + "   </td>"
                           + "   </tr>"
                        + "  </table>"
@@ -88,9 +94,10 @@ public partial class Forgot_Password : System.Web.UI.Page
                + "   </tr>"
             + " </table>";
 
-                    SendMail(SmtpServer, SmtpPort, MailFrom, DisplayNameFrom, FromPassword, MailTo, DisplayNameTo, MailCc, "", "", "", DisplayNameCc, MailBcc, Subject, MailText, Attachment);
+                    SendMail(SmtpServer, SmtpPort, MailFrom, DisplayNameFrom, FromPassword, MailTo, DisplayNameTo, MailCc, "","", "", DisplayNameCc,"", Subject,MailText, Attachment,body);
 
                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "message alert", "alert('Please check your mail ');", true);
+                    
 
                 }
                 else
@@ -118,10 +125,13 @@ public partial class Forgot_Password : System.Web.UI.Page
 
 
 
-    public static bool SendMail(string SmtpHost, int SmtpPort, string MailFrom, string DisplayNameFrom, string FromPassword, string MailTo, string DisplayNameTo, string MailCc, string mailCc2, string mailCc3, string mailCc4, string DisplayNameCc, string MailBcc, string Subject, string MailText, string Attachment)
+    public static bool SendMail(string SmtpHost, int SmtpPort, string MailFrom, string DisplayNameFrom, string FromPassword, string MailTo, string DisplayNameTo, string MailCc, string mailCc2, string mailCc3, string mailCc4, string DisplayNameCc, string MailBcc, string Subject, string MailText, string Attachment ,string body)
     {
+
         MailMessage myMessage = new MailMessage();
+        
         bool IsSucces = false;
+        
         try
         {
             myMessage.From = new MailAddress(MailFrom, DisplayNameFrom);
@@ -171,6 +181,8 @@ public partial class Forgot_Password : System.Web.UI.Page
             myMessage = null;
         }
         return IsSucces;
+
     }
+
 
 }
